c548d3ccdae4bbbd42cc5924d009b66c4280ee9d
PRASANTH SENTHIL <prasanthsenthilkumar09@gmail.com>
2026-01-28 19:39:47 +0530

    initial commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC__jcWOzrLCQUFf0E9yE0A8RVHiMFnE30",
-    authDomain: "smart-education-39570.firebaseapp.com",
-    projectId: "smart-education-39570",
-    storageBucket: "smart-education-39570.firebasestorage.app",
-    messagingSenderId: "129479378498",
-    appId: "1:129479378498:web:83e3813ce9ee06abe48fdb",
-    measurementId: "G-GJZTDBKJV5"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
